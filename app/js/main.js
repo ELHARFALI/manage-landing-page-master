b8a14bba518de33b47ded1dc33feca6eb552f935
ELHARFALI <elharfali.youssef.20@gmail.com>
2022-10-02 19:18:44 +0100

    Responsivr header part 2

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,20 @@
 const overlay = document.querySelector('.overlay');
 const navbar = document.querySelector(".navbar");
 const menu = document.querySelector(".menu");
+const img = document.querySelector('.img')
+const img2 = document.querySelector(".img2");
 
-if (menu) {
+
     menu.addEventListener("click", function () {
       navbar.classList.toggle("active");
+      img.classList.toggle('img-active')
+      img2.classList.toggle('img-isactive')
+      overlay.classList.toggle('overlay-active')
     });
-} else {
-    console.log('error');
-}
+
+overlay.addEventListener('click', function () {
+      navbar.classList.toggle("active");
+      overlay.classList.toggle("overlay-active");
+      img.classList.toggle("img-active");
+      img2.classList.toggle("img-isactive");
+    })
